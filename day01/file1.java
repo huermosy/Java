2a2d6cf77266307da47d38b16e1761d5e88792d4
huermosy <68319519+huermosy@users.noreply.github.com>
2020-11-01 14:06:53 +0800

    java day01
    这是java学习第一天所用到的文件与个人笔记，包括环境变量的配置，main函数的说明以及注释等等的基础解释。

---
+++ day01/file1.java
@@ -0,0 +1,16 @@
+class Demo
+{
+	public static void main(String[] args)
+	{
+		System.out.println("hello D-disk");
+	}
+}
+
+class Test
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello Test");
+		System.out.println("Hi");
+	}
+}
